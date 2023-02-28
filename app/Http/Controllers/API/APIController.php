056f528f84dcda85e83d94497afb2ca21983a9e2
Yunus Karaca <ynskrc82@gmail.com>
2023-03-01 01:57:54 +0300

    validasyon yazıldı

--- app/Http/Controllers/API/APIController.php
+++ app/Http/Controllers/API/APIController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Validator;
 
 class APIController extends Controller
 {
@@ -14,6 +15,24 @@ class APIController extends Controller
         if($request->isMethod('post'))
         {
             $data = $request->input();
+
+            $rules = [
+                "name" => "required",
+                "email" => "required|email|unique:users",
+                "password" => "required"
+            ];
+
+            $customMessages = [
+                "name.required" => "Bu alan zorunlu",
+                "email.required" => "Bu alan zorunlu",
+                "email.unique" => "Bu mail zaten kayıtlı",
+                "password.required" => "Bu alan zorunlu",
+            ];
+
+            $validator = Validator::make($data,$rules,$customMessages);
+            if($validator->fails()){
+                return response()->json([$validator->errors()],422);
+            }
 
             $user = new User();
             $user->name = $data["name"];
